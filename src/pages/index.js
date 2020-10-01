83e8707d16a01fb17783b0a9d4b06eb7141596d4
bassem <101010bassam@gmail.com>
2020-10-01 19:19:23 +0100

    insert the layout page inside the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Layout from '../components/layout'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Layout>
+      <p> In April, Microsoft released Visual Studio Code as part of its //Build conference.
+      I have been using it on my MBP for most of the coding I do on the Mac.
+      I found a number of things I really like…particularly doing Node and JavaScript development.
+      Best part is that it is cross platform, so I can run it on my Mac, Linux and Windows machines,
+      and I have the same editing experience. With Git support built in out of the box,
+           what more could I ask for? Well, here are the things I find save me the most time. </p>
+    </Layout>
+
+  )
 }
+
+export default Home
