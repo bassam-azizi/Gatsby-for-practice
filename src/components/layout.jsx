2582609b931b48f54e8754c2f4f9e0ed3d7930f2
bassem <101010bassam@gmail.com>
2020-10-01 19:19:01 +0100

    Create the layout page

---
+++ src/components/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
+
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      <div>{props.children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
