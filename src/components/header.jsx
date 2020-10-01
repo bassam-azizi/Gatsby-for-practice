194d99330d882c73fa016a5cc48de2017df28964
bassem <101010bassam@gmail.com>
2020-10-01 19:18:44 +0100

    create the header component

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () =>{
+    return(
+        <div>
+            <h1>Bassem Azizi</h1>
+            <ul>
+                <li>About Me</li>
+                <li>Works</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Header
