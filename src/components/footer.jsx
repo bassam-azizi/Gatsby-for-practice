e0ce85bc4955e1f74e220c23f906394caefd4d56
bassem <101010bassam@gmail.com>
2020-10-05 22:50:22 +0100

    footer updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react"
 
-
-const Footer = () =>{
-    return(
-        <div>
-            <h4>All rights are reserved</h4>
-        </div>
-    )
+const Footer = () => {
+  return (
+    <div>
+      <h4>All rights are reserved</h4>
+    </div>
+  )
 }
 
 export default Footer
