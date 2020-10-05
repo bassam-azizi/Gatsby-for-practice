2722985424e20e28c5ccb8971b1502dd3b0c2bf1
bassem <101010bassam@gmail.com>
2020-10-05 22:51:16 +0100

    ContactPage is created and updated

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <h1>Let's Talk</h1>
+            <p>contact me via <a href="101010bassam@gmail.com">Email</a></p>
+            <p>Or by calling the phone number 00216 51 110 993</p>
+        </Layout>
+    )
+}
+
+export default ContactPage
