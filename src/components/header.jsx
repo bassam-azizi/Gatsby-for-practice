f6b4ca437bbead5c334b88de1a559e0ab53227de
bassem <101010bassam@gmail.com>
2020-10-06 01:09:03 +0100

    style the h1 and link it to the home page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,44 @@
 import React from "react"
-
 import { Link } from "gatsby"
+
+import headerStyles from "./header.module.scss"
 
 const Header = () => {
   return (
-    <div>
-      <h1>Bassem Azizi</h1>
+    <div className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Bassem Azizi
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.ul}>
           <li>
-            <Link to="/aboutMe">About Me</Link>
+            <Link
+              to="/aboutMe"
+              className={headerStyles.link}
+              activeClassName={headerStyles.activeLink}
+            >
+              About Me
+            </Link>
           </li>
           <li>
-            <Link to="/work">Works</Link>
+            <Link
+              to="/work"
+              className={headerStyles.link}
+              activeClassName={headerStyles.activeLink}
+            >
+              Works
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              to="/contact"
+              className={headerStyles.link}
+              activeClassName={headerStyles.activeLink}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
