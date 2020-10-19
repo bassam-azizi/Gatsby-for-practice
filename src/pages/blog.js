fd211fff61a43ba88c80d6e29ee484a1e43c9a38
bassem <101010bassam@gmail.com>
2020-10-19 20:11:24 +0100

    query the blog post frontmatter and render them on the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
 
 const Blog = () => {
+    const data = useStaticQuery(graphql`
+        query{
+            allMarkdownRemark{
+                edges{
+                    node{
+                        frontmatter{
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+    }`)
     return (
         <Layout>
-            <p>Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit.
-                 Illum optio, autem non perspiciatis ut consequatur!</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Quidem fugit eum impedit quo magnam ab maxime vero,
-            hic accusamus incidunt fuga vitae delectus consectetur?
-                     Quisquam illo labore soluta iste natus, voluptas laudantium tempora fugiat iusto.</p>
+            <h1>Blog</h1>
+            <ol>
+                {data.allMarkdownRemark.edges.map(edge => {
+                    return (
+                        <li>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
         </Layout>
     )
 }
