2a6134e10aceace22ec9cb7165195f601f6fe239
bassem <101010bassam@gmail.com>
2020-10-19 20:14:36 +0100

    gatsby-transformer-remark plugin to transform md file has been installed and added to the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
     author: 'Andrew Mead'
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
   ]
 }
