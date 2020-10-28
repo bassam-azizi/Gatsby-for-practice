4e67cb7718789fb828df0480bc6712b98f054c72
bassem <101010bassam@gmail.com>
2020-10-28 23:36:16 +0100

    blog template page is created

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            This is a blog post
+        </Layout>
+    )
+}
+
+export default Blog
