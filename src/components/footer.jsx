ad2863b87e3ef0e3f46a4cd93c61b4c49c49c50c
bassem <101010bassam@gmail.com>
2020-11-01 14:48:23 +0100

    make space between the footer and the content of 3 rem

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 
+import footerModule from './footer.module.scss'
+
 import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
   const data = useStaticQuery(graphql`query{site{siteMetadata{title author}}}`)
   return (
-    <div>
+    <div className={footerModule.footer}>
       <h4>Created by {data.site.siteMetadata.author}, © All rights are reserved 2020</h4>
     </div>
   )
